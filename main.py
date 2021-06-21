903276a0bfa2678452894887c193be7df76362b7
luis-mederos <86256413+luis-mederos@users.noreply.github.com>
2021-06-21 14:33:34 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import SystemEvents
+hours = int(input("how many hours are you going to study"))
+print (hours,"this is how many hours you will work")
+
+mins = hours * 60
+print (mins,"this is how many minets you will work")
+seconds = mins * 60 
+print (seconds,"this is how many seconds you will work")
+
+
